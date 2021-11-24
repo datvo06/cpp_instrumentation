3ba75659f89132af5185a105eb5138739f0d5c90
datvo06 <datvo06@gmail.com>
2021-11-24 18:56:27 +0700

    Adding ast json

---
+++ dataflow.h
@@ -0,0 +1,26 @@
+#ifndef _DATAFLOW_H
+#define _DATAFLOW_H
+
+#include <llvm/IR/Instruction.h>
+#include <llvm/IR/InstrTypes.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Function.h>
+#include <list>
+#include <vector>
+
+namespace datautils {
+	typedef std::pair<llvm::Value*, std::string> node;
+	typedef std::pair<node, node> edge;
+	typedef std::list<node> node_list;
+	typedef std::list<edge> edge_list;
+
+	struct DataWorker: llvm::ModulePass {
+		static char ID;
+		DataWorker();
+		private:
+		std::dumpGlobals
+	}
+}
+
+#endif
